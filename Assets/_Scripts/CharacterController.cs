6bd983a794888dff9fa757cfeb0e851f15e67f56
Aiola <barth.13arles@hotmail.fr>
2019-08-12 17:12:12 +0200

    Initial commit

---
+++ Assets/_Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetFloat("Walk", 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetFloat("Run", 1);
+        }
+
+        animator.SetFloat("Run", 0);
+        animator.SetFloat("Walk", 0);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TeddyDream
+ Projet Perso
